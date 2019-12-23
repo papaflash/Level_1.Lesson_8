a623b8cbf75b9a0b9fd0cb990608b606eff46fbb
Aleksandr Shagov <shagov25@gmail.com>
2019-12-23 13:52:26 +0300

    Добавьте файлы проекта.

---
+++ Task_1_StructDateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+// шагов александр. Задача 1: С помощью рефлексии выведите все свойства структуры DateTime
+ 
+
+namespace Task_1_StructDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var pr in typeof(DateTime).GetProperties())
+            {
+                Console.WriteLine(pr.Name);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
